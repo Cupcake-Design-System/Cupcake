afa2b705b6c32cf064a0255836481437780de16e
Josh-Easter <josh.easter@ipreo.com>
2018-09-17 20:35:06 -0400

    better color mixins and tests

--- build/test/test_sass.js
+++ build/test/test_sass.js
@@ -1,14 +1,15 @@
 const path = require('path');
 const sassTrue = require('sass-true');
 
-const initCustomMixins = path.join(__dirname, 'init-custom-mixins.scss');
-sassTrue.runSass({file: initCustomMixins}, describe, it);
 
 const initCustomFunctions = path.join(__dirname, 'init-custom-functions.scss');
 sassTrue.runSass({file: initCustomFunctions}, describe, it);
 
-const initCustomConfig = path.join(__dirname, 'init-custom-config.scss');
-sassTrue.runSass({file: initCustomConfig}, describe, it);
+// const initCustomConfig = path.join(__dirname, 'init-custom-config.scss');
+// sassTrue.runSass({file: initCustomConfig}, describe, it);
+
+const color = path.join(__dirname, 'color.scss');
+sassTrue.runSass({file: color}, describe, it);
 
 const typography = path.join(__dirname, 'typography.scss');
 sassTrue.runSass({file: typography}, describe, it);
