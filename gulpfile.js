be5726c8478d8f973d445f16d50bf9981b72e1be
Josh-Easter <josh.easter@ipreo.com>
2019-05-08 17:58:50 -0400

    gulp working build

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,49 @@
+var gulp         = require( 'gulp' ),
+    del          = require("del"),
+    sass         = require('gulp-sass'),
+    rename       = require('gulp-rename'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
+    addHeader    = require('gulp-header'),
+    changed      = require('gulp-changed'),
+    pkg          = require('./package.json'),
+    cleanCSS     = require('gulp-clean-css'),
+    sourcemaps   = require('gulp-sourcemaps'),
+    autoprefixer = require('gulp-autoprefixer'),
+    assembler    = require('fabricator-assemble'),
+    browserSync  = require('browser-sync').create(),
+    gcmq         = require('gulp-group-css-media-queries');
 
-var gulp = require( 'gulp' ),
-    autoprefixer = require( 'gulp-autoprefixer' ),
-    browserSync  = require( 'browser-sync' ).create(),
-    assembler = require('fabricator-assemble');
-    reload  = browserSync.reload,
-    sass  = require( 'gulp-sass' ),
-    cleanCSS  = require( 'gulp-clean-css' ),
-    sourcemaps  = require( 'gulp-sourcemaps' ),
-    concat  = require( 'gulp-concat' ),
-    changed = require( 'gulp-changed' ),
-    uglify  = require( 'gulp-uglify' );
-    
 
+var metaHeader = `
+/* ----------------------- */
+/* 🎂 CUPCAKE 🎂 */
+/* pkg name: ${pkg.name} */
+/* version: ${pkg.version} */
+/* file name: <%= filename %> */
+/* ----------------------- */
+
+`;
 
 // File paths
-const files = { 
-  scssPath: 'src/scss/*.scss',
+// ------------------
+const files = {
+  scssPath: 'src/scss/',
   cssPath: 'dist/*.css',
+  distPath: 'dist/',
   scssDocsPath: 'src/docs/demo.scss',
   htmlPath: 'src/docs/**/*.html',
   jsPath: 'src/docs/assets/js/*.js'
 }
 
+// Clean
+function cleanTask() {
+  return del([files.distPath]);
+}
+
 
 // HTML
 // ------------------
-
 function htmlTask(done) {
   assembler({
 		materials: 'src/docs/core/**/*',
@@ -34,27 +52,14 @@ function htmlTask(done) {
     data: 'src/docs/data/**/*.{json,yml}',
     docs: 'src/docs/docs/**/*.md',
 		layoutIncludes: ["src/docs/views/layouts/includes/**/*", 'layout/**/*.html'],
-    dest: 'dist',
+    dest: files.distPath,
   });
   done();
 }
 
 
-gulp.task('html', (done) => {
-  assembler({
-		materials: 'src/docs/core/**/*',
-		layouts: 'src/docs/views/layouts/**/*',
-		views: ['src/docs/views/**/*', '!src/docs/views/+(layouts)/**'],
-    data: 'src/docs/data/**/*.{json,yml}',
-    docs: 'src/docs/docs/**/*.md',
-		layoutIncludes: ["src/docs/views/layouts/includes/**/*", 'layout/**/*.html'],
-    dest: 'dist',
-	})
-  done();
-});
-
-
-
+// Browser Tasks
+// ------------------
 function reloadTask(done) {
   browserSync.reload();
   done();
@@ -63,59 +68,80 @@ function reloadTask(done) {
 function serveTask(done) {
   browserSync.init({
     server: {
-      baseDir: './dist'
+      baseDir: files.distPath
     }
   });
   done();
 }
 
 
-// Used to concat the files in a specific order.
+// Scss Tasks
+// ------------------
 function scssTask() {
-  return gulp.src(files.scssPath)
+  return gulp.src(files.scssPath + '*.scss')
   .pipe(sourcemaps.init({loadMaps: true}))
   .pipe(sass({
     outputStyle: 'expanded'
   }).on('error', sass.logError))
   .pipe(autoprefixer('last 2 versions'))
+  .pipe(addHeader(metaHeader))
   .pipe(sourcemaps.write('./maps/'))
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest(files.distPath));
 }
 
+
+// Scss Docs Tasks
+// ------------------
 function scssDocsTask() {
   return gulp.src(files.scssDocsPath)
   .pipe(sass({
     outputStyle: 'expanded'
   }).on('error', sass.logError))
   .pipe(autoprefixer('last 2 versions'))
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest(files.distPath));
 }
 
-function concatCSS() {
+
+// Prod css Tasks
+// ------------------
+function prodCSS() {
   return gulp.src(files.cssPath)
-  .pipe(concat('style.min.css'))
-  .pipe(cleanCSS())
-  .pipe(gulp.dest('./dist'));
+  .pipe(gcmq())
+  .pipe(cleanCSS({rebase: false}))
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest(files.distPath));
 }
 
+
+// js Tasks
+// ------------------
 function jsTask() {
   return gulp.src(files.jsPath)
   .pipe(concat('script.min.js'))
   .pipe(uglify())
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest(files.distPath));
 }
 
-function watchTask(){
-  gulp.watch([files.scssPath, files.scssDocsPath, files.jsPath, files.htmlPath], 
-      gulp.parallel(scssTask, scssDocsTask, htmlTask, jsTask, reloadTask));    
+
+// Watch Tasks
+// ------------------
+function watchTask() {
+  gulp.watch(files.scssPath + '**/*.scss', gulp.series(scssTask, reloadTask));
+  gulp.watch(files.scssDocsPath, gulp.series(scssDocsTask, reloadTask));
+  gulp.watch(files.jsPath, gulp.series(jsTask, reloadTask));
+  gulp.watch(files.htmlPath, gulp.series(htmlTask, reloadTask));
 }
+
 
 exports.prod = gulp.series(
+  cleanTask,
+  scssDocsTask,
+  htmlTask,
   scssTask,
-  concatCSS
+  prodCSS
 );
 
-exports.default = gulp.series(
+exports.default = gulp.series(cleanTask,
   gulp.parallel(scssTask, scssDocsTask, htmlTask, jsTask),
   serveTask,
   watchTask
